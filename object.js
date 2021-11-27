b7af9879fbe3f189788a6d34690720d1b784da0f
ChiragSetia123 <90345442+ChiragSetia123@users.noreply.github.com>
2021-11-27 22:02:31 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,29 @@
+var person_1 = {
+    firstname: "chirag",
+    lastname: "setia",
+    officeaddress: "punjab"
+};
+
+console.log(person_1.firstname);
+console.log(person_1.lastname);
+console.log(person_1.officeaddress);
+
+var person_2 = {
+    firstname: "rekha",
+    lastname: "rani",
+    officeaddress: "haryana"
+};
+
+console.log(person_2.firstname);
+console.log(person_2.lastname);
+console.log(person_2.officeaddress);
+
+var person_3 = {
+    firstname: "rishi",
+    lastname: "Kumar",
+    officeaddress: "kerala"
+};
+
+console.log(person_3.firstname);
+console.log(person_3.lastname);
+console.log(person_3.officeaddress);
